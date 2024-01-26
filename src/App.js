7d277f7cd13cca14db6dbd24ac178258d9c4dd98
Rudney Santos <rudney.a.s@hotmail.com>
2024-01-26 12:21:41 -0300

    spotify react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Footer from './footer/footer';
+import Header from './header/header';
+import Playlist from './playlist/playlist';
+import Sidebar from './sidebar/siderbar';
+
+function App() {
+  return (
+    <>
+      <Footer/>
+      <Header/>
+      <Sidebar/>
+      <Playlist/>
+    </>
+  );
+}
+
+
+export default App;
